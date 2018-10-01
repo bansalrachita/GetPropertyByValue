aea39abdd6b614ad8a96ca072a79c3c7c24bf20b
rachitabansal <rachita.gemm30@gmail.com>
2018-10-01 15:23:21 -0400

    adding functions to find object property and return the values.

---
+++ getValueOfProperty.js
@@ -0,0 +1,49 @@
+/**
+ * @param {object} list the list of objects or object.
+ * @param {string} keyName name of the property to search for.
+ * */
+function getPropertyByValue(list, keyName){
+    let result = [];
+
+    if(!keyName){
+        return null;
+    }
+
+    if(typeof (list) === "object" && !Array.isArray(list)){
+        result = helper(list, keyName);
+    }else {
+        for(let obj of list){
+            result.push(helper(obj, keyName));
+        }
+    }
+
+    return result;
+}
+
+/**
+ * @param {object} obj the object to search property.
+ * @param {string} keyName name of the property to search for.
+ * */
+function helper(obj, keyName){
+    let result = null;
+
+    if(typeof (obj) === "object" && !Array.isArray(obj)){
+        for(let value in obj){
+            if(value === keyName){
+                result = obj[value];
+                break;
+            }else {
+                result = helper(obj[value], keyName);
+            }
+        }
+    }
+    return result;
+}
+const time = new Date().getMilliseconds();
+console.log("getPropertyByValue: ", getPropertyByValue({}, "username"));
+const endTime = new Date().getMilliseconds();
+console.log("total time : ", endTime - time);
+
+exports = {
+    getPropertyByValue: getPropertyByValue
+};
